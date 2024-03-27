2c2e1350e3d8b173868336b4fa3bf07db499c4f0
Tarek Chaalan <tchaalan23@icloud.com>
2024-03-27 06:13:34 -0700

    Fixed issue where time would continue counting between sessions

--- auth/main.js
+++ auth/main.js
@@ -1,3 +1,4 @@
+// Function to toggle between signup and login forms
 function toggleForms() {
   const signupForm = document.getElementById("signup-form");
   const loginForm = document.getElementById("login-form");
@@ -5,18 +6,20 @@ function toggleForms() {
   loginForm.classList.toggle("active");
 }
 
-// Handle Signup
+// Handle Signup form submission
 document
   .getElementById("signup-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
     const username = document.getElementById("signup-username").value;
     const password = document.getElementById("signup-password").value;
+
+    // Store username and password in localStorage
     localStorage.setItem(username, password);
     alert("Signup successful. Please log in.");
   });
 
-// Handle Login
+// Handle Login form submission
 document
   .getElementById("login-form")
   .addEventListener("submit", function (event) {
@@ -24,8 +27,10 @@ document
     const username = document.getElementById("login-username").value;
     const password = document.getElementById("login-password").value;
     const storedPassword = localStorage.getItem(username);
+
+    // Check if entered password matches stored password and redirect to quiz page
     if (password === storedPassword) {
-      // Redirect to the quiz page if login is successful
+      localStorage.clear();
       window.location.href = "../quiz/quiz.html";
     } else {
       alert("Invalid username or password.");
